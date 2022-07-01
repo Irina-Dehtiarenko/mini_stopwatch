5c89e557dd468d327c0a2e252e52377b026b1596
Irina-Dehtiarenko <93826301+Irina-Dehtiarenko@users.noreply.github.com>
2022-07-01 20:54:11 +0200

    created a button click service for a full time reset

--- main.js
+++ main.js
@@ -1,38 +1,44 @@
 const spanTime = document.querySelector('span.s')
 const buttonStart = document.querySelector('header button.start')
+const buttonReset = document.querySelector('button.reset')
 
 let startTime = 0.00
 let interval
-
 
 const startStoper = () => {
 	spanTime.textContent = startTime
 	goTime = startTime += 0.01
 	spanTime.textContent = startTime.toFixed(2)
-
 }
 
 const start = () => {
 	interval = setInterval(startStoper, 10)
-
 	buttonStart.classList.remove('start')
 	buttonStart.classList.add('stop')
 	buttonStart.textContent = 'Pauza'
-
 }
 
-
 const stop = () => {
-
 	buttonStart.classList.remove('stop')
 	buttonStart.classList.add('start')
 	buttonStart.textContent = 'Start'
 	clearInterval(interval)
-
 }
 
+const reset = () => {
+	console.log('reset')
+	startTime = 0.00
+	spanTime.textContent = '---'
+	if (buttonStart.className === 'stop') {
+		buttonStart.classList.remove('stop')
+		buttonStart.classList.add('start')
+		buttonStart.textContent = 'Start'
+	}
+	clearInterval(interval)
+}
 
 buttonStart.addEventListener('click', () => buttonStart.className === 'start' ? start() : stop())
 
+buttonReset.addEventListener('click', reset)
 
 
